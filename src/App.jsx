4e4f05ef8840957b6ad2e8130f8c02588e3d1da1
Anish Patel <apatel6699@gmail.com>
2019-12-06 16:11:31 -0500

    Added Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden h-screen">
-      <div id="header" className="flex-none">
-        <div className="w-full h-16 bg-gray-800"></div>
-      </div>
+      <Header />
 
       <div id="main" className="flex-grow container mx-auto flex flex-col overflow-y-scroll">
       </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id="header" className="flex-none">
+      <div className="w-full h-16 bg-gray-800"></div>
+    </div>
+  );
+};
+
+export default Header;
