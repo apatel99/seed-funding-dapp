d9ce6896c5120a3cc6061af175060848a022a9a2
Anish Patel <apatel6699@gmail.com>
2019-12-08 20:00:32 -0500

    Component to add investors

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CreateSeeding from './components/CreateSeeding';
+import AddInvestor from './components/AddInvestor';
 
 const App = () => {
   return (
@@ -11,8 +12,7 @@ const App = () => {
       <div id="main" className="flex-grow container mx-auto flex flex-col overflow-y-scroll">
         <CreateSeeding/>
 
-      <div id="footer" className="flex-none">
-        <div className="w-full h-16 bg-gray-800"></div>
+        <AddInvestor/>
       </div>
       
       <Footer />
---
+++ src/components/AddInvestor.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { ethers } from 'ethers';
+import abi from '../utils/abi';
+
+class Addinvestor extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            Address:"",
+            Id:"",
+        };
+
+        this.handlechange=this.handlechange.bind(this);
+        this.handlesubmit=this.handlesubmit.bind(this);
+    };
+
+    handlechange = (e) => {
+        this.setState({
+            [e.target.name]:e.target.value
+        },console.log(this.state))
+    };
+
+    handlesubmit = async(e) => {
+        e.preventDefault();
+        let ethereum = window.ethereum;
+        let addr = await ethereum.enable();
+        let  provider = new ethers.providers.Web3Provider(window.web3.currentProvider);
+        const signer = provider.getSigner();
+        
+        let address = "0x6abf86c454134bd21fb13b2aa44046b9afdf682f";
+        let contract = new ethers.Contract(address, abi, signer);
+        let tx1 = await contract.getaddInvestorbyId(this.state.Id,this.state.Address);
+    }
+
+    render() {
+        return (
+            <div className="mt-4">
+                <h4 className="text-3xl font-bold">Hey Investor, register here!</h4>
+
+                <form
+                    className="flex p-4 justify-between align-items-center"
+                    onSubmit={this.handlesubmit}
+                >
+                    <div className="pr-2 flex-col">
+                        <div className="text-xl">
+                            <label htmlFor="Address" className="pr-2">Please enter your ETH address</label>
+                            <input
+                                type="text"
+                                name="Address"
+                                onChange={this.handlechange}
+                                value={this.state.Address}
+                                placeholder="ETH Address"
+                                className="px-2 border-b-2 border-gray-800 rounded-lg"
+                            />
+                        </div>
+
+                        <div className="text-xl">
+                            <label htmlFor="Id" className="pr-2">Please enter the contract ID to invest in</label>
+                            <input
+                                type="text"
+                                name="Id"
+                                label="Id"
+                                onChange={this.handlechange}
+                                value={this.state.Id}
+                                placeholder="Ex 121"
+                                className="px-2 border-b-2 border-gray-800 rounded-lg"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="pl-2">
+                        <button
+                            className="px-8 py-2 bg-blue-800 rounded-full uppercase tracking-wide font-bold text-white"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+};
+
+export default Addinvestor;
