4a3afc00f6766f044c9164e2f0c90de2efe16bb6
Anish Patel <apatel6699@gmail.com>
2019-12-09 08:42:05 -0500

    Content added to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div id="footer" className="flex-none">
-      <div className="w-full h-16 bg-gray-800"></div>
+    <div id="footer" className="flex justify-center align-items-center w-full h-16 bg-gray-800 py-4">
+      <p className="text-white text-md">&copy; Anish Patel (101227442)</p>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div id="header" className="flex-none">
-      <div className="w-full h-16 bg-gray-800"></div>
+    <div id="header" className="flex justify-center align-items-center w-full bg-gray-800 py-4">
+      <p className="text-white uppercase tracking-wide font-bold text-3xl">Seed Funding App</p>
     </div>
   );
 };
